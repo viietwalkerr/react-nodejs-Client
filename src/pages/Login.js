9be47a659331298bb542ce1fcc4bad00386dd36c
Nathan <nathanthai1@hotmail.com>
2021-08-13 21:50:29 +1000

    moved cross domain

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -28,7 +28,7 @@ function Login() {
 
     const login = () => {
         const data = {username: username, password: password}; //creating an object
-        axios.post("https://illumin8.netlify.app/" + "auth/login", {crossDomain: true}, data
+        axios.post("https://illumin8.netlify.app/" + "auth/login", data, {crossDomain: true}
         )
         .then((response) => {
             console.log(response.data)
